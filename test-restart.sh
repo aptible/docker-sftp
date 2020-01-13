5230ed3d25f17383cc8af6b11dd3a637018e42ce
Alex Kubacki <alex@aptible.com>
2020-01-13 15:36:34 -0500

    Clean up test cases.

--- test-restart.sh
+++ test-restart.sh
@@ -45,8 +45,6 @@ docker run -d --name="$DB_CONTAINER" \
 echo "Waiting for DB to come online"
 wait_for_db
 
-docker exec "$DB_CONTAINER" mkdir /etc-backup/ssh
-
 echo "Creating additional users."
 docker exec "$DB_CONTAINER" add-sftp-user foo fee
 docker exec "$DB_CONTAINER" useradd bar
@@ -54,28 +52,39 @@ docker exec "$DB_CONTAINER" useradd bar
 echo "Modify the sshd config."
 docker exec "$DB_CONTAINER" bash -c "echo '#foobar' >> /etc/ssh/sshd_config"
 
-echo "Persist the sshd config."
-docker exec "$DB_CONTAINER" cp /etc/ssh/sshd_config /etc-backup/ssh/
-
 echo "Stopping DB"
 docker stop "$DB_CONTAINER" >/dev/null 2>&1
 
 echo "Check that SSHD was terminated."
 docker logs "$DB_CONTAINER" | grep "Received signal 15; terminating." >/dev/null 2>&1
 
+echo "Esnure we got warned that an sshd_config was modified"
+docker logs "$DB_CONTAINER" | grep 'WARNING - changes detected in /etc/ssh/sshd_config' >/dev/null 2>&1
+
 echo "Simulate deploy restart (volume persists, container recycled)"
 docker rm "$DB_CONTAINER" >/dev/null
-docker run -d --rm --name="$DB_CONTAINER" \
+docker run -d --name="$DB_CONTAINER" \
   --volumes-from "$DATA_CONTAINER" \
   "$IMG" >/dev/null 2>&1
-
 
 echo "Checking the additional users do exist."
 docker exec "$DB_CONTAINER" id foo >/dev/null 2>&1
 docker exec "$DB_CONTAINER" id bar >/dev/null 2>&1
 
-echo "Ensure the sshd file is not still modified"
-docker exec "$DB_CONTAINER" grep "foobar" /etc/ssh/sshd_config >/dev/null 2>&1
+echo "Check the sshd_config modifications were lost."
+! docker exec "$DB_CONTAINER" grep "foobar" /etc/ssh/sshd_config >/dev/null 2>&1
 
-echo "Esnure we got warned that an un-expected sshd_config file was used"
-docker logs "$DB_CONTAINER" | grep 'WARNING: unexpected' >/dev/null 2>&1
+echo "Modify and persist the sshd_config."
+docker exec "$DB_CONTAINER" mkdir /etc-backup/ssh
+docker exec "$DB_CONTAINER" bash -c "echo '#foobar' >> /etc/ssh/sshd_config"
+docker exec "$DB_CONTAINER" cp /etc/ssh/sshd_config /etc-backup/ssh/
+
+echo "Simulate deploy restart (volume persists, container recycled)"
+docker stop "$DB_CONTAINER" >/dev/null 2>&1
+docker rm "$DB_CONTAINER" >/dev/null
+docker run -d --rm --name="$DB_CONTAINER" \
+  --volumes-from "$DATA_CONTAINER" \
+  "$IMG" >/dev/null 2>&1
+
+echo "Ensure the sshd file is still modified"
+docker exec "$DB_CONTAINER" grep "foobar" /etc/ssh/sshd_config >/dev/null 2>&1
